fdc7820e434f7cd9eff30423c3958a6009b55645
delwer72 <mddelwerhussain93@gmail.com>
2026-05-04 00:55:06 +0600

    updated

--- src/proxy.js
+++ src/proxy.js
@@ -16,3 +16,26 @@ export async function proxy(request) {
 export const config = {
   matcher: ["/profile", "/showallanimals/:path"],
 };
+
+// import { NextResponse } from "next/server";
+// import { auth } from "./lib/auth";
+// import { headers } from "next/headers";
+
+// export async function proxy(request) {
+//   const session = await auth.api.getSession({
+//     headers: await headers(),
+//   });
+
+//   if (!session) {
+//     const loginUrl = new URL('/login', request.url);
+//     loginUrl.searchParams.set("callbackUrl", request.url);
+
+//     return NextResponse.redirect(loginUrl);
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/profile", "/showallanimals/:path"],
+// };
